8c3ecb86e22dceccf15d3b7a5433bd4e4865f5ea
sanlouise <sandra.adamshallie@gmail.com>
2017-07-27 17:44:06 -0400

    Filter words into easy, normal and hard arrays

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.set('view engine', 'mustache');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Get all words from file system
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+
+//Filter words into easy, normal and hard arrays
+const easyWords = words.filter(word => word.length >= 4 && word.length <= 6)
+const normalWords = words.filter(word => word.length >= 6 && word.length <= 8)
+const hardWords = words.filter(word => word.length >= 8)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
