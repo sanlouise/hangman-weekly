33f64e5435935349dd9f396db9bb947d631a592f
sanlouise <sandra.adamshallie@gmail.com>
2017-07-27 18:22:04 -0400

    Create logic to pick random word from word arrays

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ const easyWords = words.filter(word => word.length >= 4 && word.length <= 6)
 const normalWords = words.filter(word => word.length >= 6 && word.length <= 8)
 const hardWords = words.filter(word => word.length >= 8)
 
+//Get a random number in range to call on word arrays.
+//found on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Grab a random word from array
+const easyWord = easyWords[getRandomInt(0, easyWords.length)];
+const normalWord = normalWords[getRandomInt(0, normalWords.length)];
+const hardWord = hardWords[getRandomInt(0, hardWords.length)];
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 })
